8dfd7117a65ec8bc20c9a5b371895bb10cc3685b
Mike <Cuestamichael0@gmail.com>
2023-05-22 19:59:27 -0400

    created task-4 for fibonacci seq

---
+++ task-4.py
@@ -0,0 +1,12 @@
+# Task 4
+# Fibonacci Series: Write a program to generate the Fibonacci sequence 
+# up to a specified number of terms. The Fibonacci sequence starts with 0 and 1,
+#  and each subsequent number is the sum of the two preceding numbers.
+
+import os
+from functions import *
+
+# fib seq will always start with 0, 1
+
+# create a loop that will do equation up until the specified number of terms
+
