7772bc215e380397d2a862c854da2af9f169d351
Mike <Cuestamichael0@gmail.com>
2023-05-22 23:10:57 -0400

    Created task-6

---
+++ task-6.py
@@ -0,0 +1,8 @@
+'''
+Temperature Converter: Build a program that converts temperature values
+between Fahrenheit and Celsius. The user should be able to enter a 
+temperature in one unit and get the converted value in the other unit.
+'''
+import os
+from functions import *
+os.system('clear && neofetch')
